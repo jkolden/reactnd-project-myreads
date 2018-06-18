4141222e0f06b4de927be9896f7b580b4c9be99a
John Kolden <john.kolden@gmail.com>
2018-06-18 15:40:06 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
+import Search from './Search'
+import { Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 
 
 
 
 class BooksApp extends React.Component {
   state = {
+    screen: 'list',
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -21,34 +26,21 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+      <Route exact path='/' render={() => (
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (<div><ListBooks
-                  />
+        <div>
+        <ListBooks />
           <div className="open-search">
-            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-          </div></div>
+            <Link to='/search'>Add a book</Link>
+          </div>
+          </div>
 
-        )}
+        )} />
+
+      <Route path='/search' component={Search} />
+
+
+      
       </div>
 
     )
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react'
 //import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
+import { Link } from 'react-router-dom'
+
 
 
 //https://stackoverflow.com/questions/39195687/setting-a-backgroundimage-with-react-inline-styles
@@ -29,26 +31,26 @@ class ListBooks extends Component {
       })
   }
 
-  updateShelf = (book, e) => {
-  let savedEvent = e;
-  let savedTarget = e.target.value; //https://stackoverflow.com/questions/42089795/reactjs-cant-set-state-from-an-event-with-event-persist
-  BooksAPI.update(book, savedTarget).then((resp) => {
+      updateShelf = (book, e) => {
+      let savedEvent = e;
+      let savedTarget = e.target.value; //https://stackoverflow.com/questions/42089795/reactjs-cant-set-state-from-an-event-with-event-persist
+      BooksAPI.update(book, savedTarget).then((resp) => {
 
-  let items = this.state.books;
-  console.log(items);
-  let objIndex = items.findIndex((obj => obj.id == book.id));
+      let items = this.state.books;
+      console.log(items);
+      let objIndex = items.findIndex((obj => obj.id == book.id));
 
 
-  console.log(items[objIndex].shelf);
+      console.log(items[objIndex].shelf);
 
-  items[objIndex].shelf = savedTarget;
-  console.log(items);
+      items[objIndex].shelf = savedTarget;
+      console.log(items);
 
-  // update state
-    this.setState({
-        items
-    });
-})
+      // update state
+          this.setState({
+              items
+          });
+    })
 
    
 };
@@ -65,6 +67,20 @@ class ListBooks extends Component {
       <div className="list-books-content">
         <div>
           <div className="bookshelf">
+
+           <h2 className="bookshelf-title">Currently Reading</h2>
+            <div className="bookshelf-books">
+
+              <ol className="books-grid">
+                {this.state.books.filter((book) => book.shelf === 'currentlyReading').map((book) => (<li key={book.id}>
+                  <Book book={book} updateShelf={this.updateShelf} />
+
+                </li>))
+
+                }
+
+              </ol>
+            </div>
 
             <h2 className="bookshelf-title">Read</h2>
             <div className="bookshelf-books">
